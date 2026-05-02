d9f8a2a405a034c4573d9b51682a72e3818fc3a7
marjunhingpit21-svg <marjunhingpit21@gmail.com>
2026-05-02 20:50:35 +0800

    just small css changes

--- login.php
+++ login.php
@@ -41,11 +41,9 @@
             
             <form action="" method="post" id="loginForm">
                 <div class="field input">
-                    <label for="Username"><i class="fa fa-user" style="font-size:32px;"></i></label>
                     <input type="text" name="Username" id="Username" placeholder="username" required>
                 </div>
                 <div class="field input">
-                    <label for="Password"><i class="fa fa-unlock-alt" id="passwordIcon" style="font-size:32px;"></i></label>
                     <input type="password" name="Password" id="Password" placeholder="password" required>
                 </div>
                 <div class="button">
